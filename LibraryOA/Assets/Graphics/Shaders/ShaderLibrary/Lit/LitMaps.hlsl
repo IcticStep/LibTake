7426d23ff4ab167b0ed227be983378ab897508a6
narberalAntares <valerii.deineka.volodymyrovych@gmail.com>
2023-09-29 19:41:39 +0300

    Migrated and fixed for lts

---
+++ LibraryOA/Assets/Graphics/Shaders/ShaderLibrary/Lit/LitMaps.hlsl
@@ -0,0 +1,6 @@
+TEXTURE2D(_BaseMap);            SAMPLER(sampler_BaseMap);
+float4 _BaseMap_TexelSize;
+float4 _BaseMap_MipInfo;
+TEXTURE2D(_SpecularColorMap);   SAMPLER(sampler_SpecularColorMap);
+TEXTURE2D(_BumpMap);            SAMPLER(sampler_BumpMap);
+TEXTURE2D(_EmissionMap);        SAMPLER(sampler_EmissionMap);
