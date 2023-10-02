d5fb2fe65782cc482abbce5deb884aecd8a506f7
Maxim Moskalyk <ictic.step@gmail.com>
2023-10-02 22:08:21 +0300

    Player inertia movement.

--- LibraryOA/Assets/Code/Runtime/Player/PlayerMove.cs
+++ LibraryOA/Assets/Code/Runtime/Player/PlayerMove.cs
@@ -1,5 +1,6 @@
 using Code.Runtime.Services.InputService;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Zenject;
 
 namespace Code.Runtime.Player
@@ -7,10 +8,15 @@ namespace Code.Runtime.Player
     [RequireComponent(typeof(CharacterController))]
     internal sealed class PlayerMove : MonoBehaviour
     {
-        [SerializeField] private float _speed;
+        private const float MinimalInputThreshold = 0.001f;
+        
+        [FormerlySerializedAs("_speed")] [SerializeField] private float _baseSpeed = 10f;
+        [SerializeField] private float _inertiaDecayRate = 2f;
+        
         private IInputService _input;
         private Camera _camera;
         private CharacterController _characterController;
+        private Vector3 _currentVelocity;
 
         private void Awake()
         {
@@ -26,11 +32,31 @@ namespace Code.Runtime.Player
         {
             Vector2 input = _input.GetMovement();
             
-            Vector3 movementVector = _camera.transform.TransformDirection(input);
-            movementVector.y = 0;
-            movementVector.Normalize();
-
-            _characterController.Move(Time.deltaTime * _speed * movementVector);
+            Vector3 direction = GetMovementDirection(input);
+            _currentVelocity = CalculateVelocity(input, direction);
+            ApplyVelocity();
         }
+
+        private Vector3 GetMovementDirection(Vector2 input)
+        {
+            Vector3 movementDirection = _camera.transform.TransformDirection(input);
+            movementDirection.y = 0;
+            movementDirection.Normalize();
+            return movementDirection;
+        }
+
+        private Vector3 CalculateVelocity(Vector2 input, Vector3 direction)
+        {
+            if (HasInput(input))
+                return _baseSpeed * direction;
+            
+            return Vector3.Lerp(_currentVelocity, Vector3.zero, _inertiaDecayRate * Time.deltaTime);
+        }
+
+        private static bool HasInput(Vector2 input) =>
+            input.magnitude > MinimalInputThreshold;
+
+        private void ApplyVelocity() =>
+            _characterController.Move(_currentVelocity * Time.deltaTime);
     }
 }
