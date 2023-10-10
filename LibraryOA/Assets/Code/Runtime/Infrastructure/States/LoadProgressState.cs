0c095b8f8c6f0816566b8d4530015ffac467a62e
Maxim Moskalyk <ictic.step@gmail.com>
2023-10-10 22:20:59 +0300

    Payer progress intializing.

--- LibraryOA/Assets/Code/Runtime/Infrastructure/States/LoadProgressState.cs
+++ LibraryOA/Assets/Code/Runtime/Infrastructure/States/LoadProgressState.cs
@@ -1,13 +1,41 @@
+using Code.Runtime.Data;
+using Code.Runtime.Infrastructure.Services.PersistentProgress;
+using Code.Runtime.Infrastructure.Services.SaveLoad;
 using Code.Runtime.Infrastructure.States.Api;
 
 namespace Code.Runtime.Infrastructure.States
 {
     internal sealed class LoadProgressState : IState
     {
-        public void Exit() =>
-            throw new System.NotImplementedException();
+        private const string MainSceneName = "Library";
+        
+        private readonly GameStateMachine _stateMachine;
+        private readonly IPlayerProgressService _playerProgressService;
+        private readonly ISaveLoadService _saveLoadService;
 
-        public void Start() =>
-            throw new System.NotImplementedException();
+        public LoadProgressState(GameStateMachine stateMachine, IPlayerProgressService playerProgressService, ISaveLoadService saveLoadService)
+        {
+            _stateMachine = stateMachine;
+            _playerProgressService = playerProgressService;
+            _saveLoadService = saveLoadService;
+        }
+
+        public void Start()
+        {
+            LoadProgressOrCreateNew();
+            _stateMachine.EnterState<LoadLevelState, string>(MainSceneName);
+        }
+
+        private void LoadProgressOrCreateNew() =>
+            _playerProgressService.Progress = 
+                _saveLoadService.LoadProgress()
+                ?? CreateNewProgress();
+
+        private PlayerProgress CreateNewProgress() =>
+            new();
+
+        public void Exit()
+        {
+        }
     }
 }
