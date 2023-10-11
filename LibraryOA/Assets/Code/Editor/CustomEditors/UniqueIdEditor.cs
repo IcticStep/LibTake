7d6f969665e3ef90ffe21bd0ba16d39cf5769d4e
Maxim Moskalyk <ictic.step@gmail.com>
2023-10-11 10:38:44 +0300

    Unique id logic implemented.

---
+++ LibraryOA/Assets/Code/Editor/CustomEditors/UniqueIdEditor.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Linq;
+using Code.Runtime.Logic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Code.Editor.CustomEditors
+{
+    [CustomEditor(typeof(UniqueId))]
+    internal sealed class UniqueIdEditor : UnityEditor.Editor
+    {
+        private void OnEnable()
+        {
+            UniqueId uniqueId = (UniqueId) target;
+      
+            if(IsPrefab(uniqueId))
+                return;
+
+            if(HasNoId(uniqueId) || HasPrefabId(uniqueId))
+            {
+                Generate(uniqueId);
+                return;
+            }
+
+            if(IsIsUnique(uniqueId))
+                return;
+
+            Generate(uniqueId);
+        }
+
+        private static bool HasPrefabId(UniqueId uniqueId) =>
+            uniqueId.Id[0] == '_';
+
+        private static bool HasNoId(UniqueId uniqueId) =>
+            string.IsNullOrEmpty(uniqueId.Id);
+
+        private bool IsPrefab(UniqueId uniqueId) => 
+            uniqueId.gameObject.scene.rootCount == 0;
+
+        private bool IsIsUnique(UniqueId uniqueId)
+        {
+            UniqueId[] uniqueIds = FindObjectsOfType<UniqueId>();
+            return !uniqueIds.Any(other => other != uniqueId && other.Id == uniqueId.Id);
+        }
+
+        private void Generate(UniqueId uniqueId)
+        {
+            (uniqueId as IUniqueIdInitializer).Id = $"{uniqueId.gameObject.scene.name}_{Guid.NewGuid().ToString()}";
+
+            if(Application.isPlaying)
+                return;
+
+            EditorUtility.SetDirty(uniqueId);
+            EditorSceneManager.MarkSceneDirty(uniqueId.gameObject.scene);
+        }
+    }
+}
