7d6572b6d594c0d1da46d77510d42b6c37f26e68
Maxim Moskalyk <ictic.step@gmail.com>
2023-10-20 23:22:07 +0300

    Player initial spawn editor, book slot editor adjusted.

--- LibraryOA/Assets/Code/Editor/Editors/PlayerInitialSpawnEditor.cs
+++ LibraryOA/Assets/Code/Editor/Editors/PlayerInitialSpawnEditor.cs
@@ -1,18 +1,17 @@
-using Code.Runtime.Logic.Interactions;
 using Code.Runtime.Logic.SpawnMarkers;
 using UnityEditor;
 using UnityEngine;
 
 namespace Code.Editor.Editors
 {
-    [CustomEditor(typeof(BookSlotSpawn))]
-    internal sealed class BookSlotSpawnerEditor : UnityEditor.Editor
+    [CustomEditor(typeof(PlayerInitialSpawn))]
+    public sealed class PlayerInitialSpawnEditor : UnityEditor.Editor
     {
         [DrawGizmo(GizmoType.Active | GizmoType.Pickable | GizmoType.NonSelected | GizmoType.Selected )]
-        public static void RenderCustomGizmo(BookSlotSpawn spawn, GizmoType gizmo)
+        public static void RenderCustomGizmo(PlayerInitialSpawn spawn, GizmoType gizmo)
         {
             Color previousColor = Gizmos.color;
-            Gizmos.color = Color.red;
+            Gizmos.color = Color.yellow;
             Gizmos.DrawSphere(spawn.transform.position, 0.5f);
             Gizmos.color = previousColor;
         }
