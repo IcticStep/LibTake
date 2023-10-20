a7a888a118f53ec71a337d54a5696de4a61fe9fb
Maxim Moskalyk <ictic.step@gmail.com>
2023-10-20 21:25:27 +0300

    Unique id is readonly in inspector right now.

--- LibraryOA/Assets/Code/Runtime/Logic/UniqueId.cs
+++ LibraryOA/Assets/Code/Runtime/Logic/UniqueId.cs
@@ -1,10 +1,11 @@
+using Code.Runtime.Utils;
 using UnityEngine;
 
 namespace Code.Runtime.Logic
 {
     public sealed class UniqueId : MonoBehaviour
     {
-        [SerializeField] private string _id;
+        [ReadOnly][SerializeField] private string _id;
         public string Id => _id;
 
         public void InitId(string id) =>
