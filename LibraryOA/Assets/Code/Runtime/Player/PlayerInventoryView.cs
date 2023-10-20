5c34a8a96bf40a01558b1d438a04f83eac3842c7
Maxim Moskalyk <ictic.step@gmail.com>
2023-10-20 21:42:39 +0300

    Book views updated to show actual book type.

--- LibraryOA/Assets/Code/Runtime/Player/PlayerInventoryView.cs
+++ LibraryOA/Assets/Code/Runtime/Player/PlayerInventoryView.cs
@@ -1,4 +1,6 @@
+using Code.Runtime.Infrastructure.Services.StaticData;
 using Code.Runtime.Services.Player;
+using Code.Runtime.StaticData;
 using UnityEngine;
 using Zenject;
 
@@ -8,21 +10,49 @@ namespace Code.Runtime.Player
     {
         [SerializeField] private GameObject _bookObject;
         private IPlayerInventoryService _playerInventoryService;
- 
+        private MeshRenderer _bookMeshRenderer;
+        private IStaticDataService _staticData;
+
         [Inject]
-        private void Construct(IPlayerInventoryService playerInventoryService) =>
+        private void Construct(IPlayerInventoryService playerInventoryService, IStaticDataService staticDataService)
+        {
             _playerInventoryService = playerInventoryService;
+            _staticData = staticDataService;
+        }
 
         private void Awake() =>
             _playerInventoryService.Updated += UpdateView;
 
-        private void Start() =>
+        private void Start()
+        {
+            _bookMeshRenderer = _bookObject.GetComponent<MeshRenderer>();
             UpdateView();
+        }
 
         private void OnDestroy() =>
             _playerInventoryService.Updated -= UpdateView;
 
-        private void UpdateView() =>
+        private void UpdateView()
+        {
+            SetMaterialIfAny();
             _bookObject.SetActive(_playerInventoryService.HasBook);
+        }
+
+        private void SetMaterialIfAny()
+        {
+            Material material = GetBookMaterial();
+            if(material is not null)
+                _bookMeshRenderer.material = material;
+        }
+
+        private Material GetBookMaterial()
+        {
+            string bookId = _playerInventoryService.CurrentBookId;
+            if(string.IsNullOrWhiteSpace(bookId))
+                return null;
+            
+            StaticBook data = _staticData.GetBookData(bookId);
+            return data.StaticBookType.Material;
+        }
     }
 }
