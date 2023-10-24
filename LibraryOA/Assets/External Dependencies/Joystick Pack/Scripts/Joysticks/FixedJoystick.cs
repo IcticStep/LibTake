716c670cb5d29764ebaef3e8b90ac8d14940d5b2
Maxim Moskalyk <ictic.step@gmail.com>
2023-10-25 00:00:58 +0300

    Joystick Pack imported into project.

---
+++ LibraryOA/Assets/External Dependencies/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
@@ -0,0 +1,9 @@
+﻿using External_Dependencies.Joystick_Pack.Scripts.Base;
+
+namespace External_Dependencies.Joystick_Pack.Scripts.Joysticks
+{
+    public class FixedJoystick : Joystick
+    {
+
+    }
+}
