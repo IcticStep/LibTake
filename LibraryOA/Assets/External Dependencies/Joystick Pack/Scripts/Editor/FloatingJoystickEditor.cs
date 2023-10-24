22ebb5195f139e615f23cdd667685d085fffd9ec
Maxim Moskalyk <ictic.step@gmail.com>
2023-10-25 00:00:58 +0300

    Joystick Pack imported into project.

---
+++ LibraryOA/Assets/External Dependencies/Joystick Pack/Scripts/Editor/FloatingJoystickEditor.cs
@@ -0,0 +1,23 @@
+﻿using External_Dependencies.Joystick_Pack.Scripts.Joysticks;
+using UnityEditor;
+using UnityEngine;
+
+namespace External_Dependencies.Joystick_Pack.Scripts.Editor
+{
+    [CustomEditor(typeof(FloatingJoystick))]
+    public class FloatingJoystickEditor : JoystickEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (background != null)
+            {
+                RectTransform backgroundRect = (RectTransform)background.objectReferenceValue;
+                backgroundRect.anchorMax = Vector2.zero;
+                backgroundRect.anchorMin = Vector2.zero;
+                backgroundRect.pivot = center;
+            }
+        }
+    }
+}
