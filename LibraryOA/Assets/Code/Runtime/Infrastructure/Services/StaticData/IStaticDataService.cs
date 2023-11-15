5b2f627f291628eaef3c83707e735867775aadf8
Maxim Moskalyk <ictic.step@gmail.com>
2023-11-15 20:54:39 +0200

    Start scene is now read from startup settings.

--- LibraryOA/Assets/Code/Runtime/Infrastructure/Services/StaticData/IStaticDataService.cs
+++ LibraryOA/Assets/Code/Runtime/Infrastructure/Services/StaticData/IStaticDataService.cs
@@ -5,10 +5,12 @@ namespace Code.Runtime.Infrastructure.Services.StaticData
     public interface IStaticDataService
     {
         StaticReadingTable ReadingTableData { get; }
+        StartupSettings StartupSettings { get; }
         void LoadAll();
         void LoadBooks();
         void LoadLevels();
         void LoadInteractables();
+        void LoadStartupSettings();
         StaticBook ForBook(string id);
         LevelStaticData ForLevel(string key);
     }
