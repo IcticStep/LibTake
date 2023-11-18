12dbd605746bf868c6185d61411b9ae70d8f4782
Maxim Moskalyk <ictic.step@gmail.com>
2023-11-18 21:45:39 +0200

    Progress bar background added.

--- LibraryOA/Assets/Code/Runtime/Ui/ProgressBar.cs
+++ LibraryOA/Assets/Code/Runtime/Ui/ProgressBar.cs
@@ -7,8 +7,25 @@ namespace Code.Runtime.Ui
     {
         [SerializeField]
         private Image _target;
+        [SerializeField]
+        private Image _background;
+        [SerializeField]
+        private float _visualMinimum = 0.01f;
 
-        public void SetProgress(float value, float maxValue) =>
+        public void SetProgress(float value, float maxValue)
+        {
             _target.fillAmount = value / maxValue;
+            SetVisibility(value, maxValue);
+        }
+
+        private void SetVisibility(float value, float maxValue)
+        {
+            bool shouldBeVisible = ValueIsMoreThanVisualMinimum(value, maxValue);
+            _background.enabled = shouldBeVisible;
+            _target.enabled = shouldBeVisible;
+        }
+
+        private bool ValueIsMoreThanVisualMinimum(float value, float maxValue) =>
+            value / maxValue >= _visualMinimum;
     }
 }
