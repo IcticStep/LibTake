9c9f7a90b8fd049ecfeb91fcd3606b32e7facb94
Maxim Moskalyk <ictic.step@gmail.com>
2023-11-19 21:40:11 +0200

    Resources folder structure fixed; truck collider added.

--- LibraryOA/Assets/Code/Runtime/Infrastructure/AssetManagement/AssetPath.cs
+++ LibraryOA/Assets/Code/Runtime/Infrastructure/AssetManagement/AssetPath.cs
@@ -2,9 +2,9 @@ namespace Code.Runtime.Infrastructure.AssetManagement
 {
     internal static class AssetPath
     {
-        public const string Player = "Player/Player";
-        public const string BookSlot = "Interactables/BookSlot";
-        public const string ReadingTable = "Interactables/ReadingTable";
+        public const string Player = "Level Objects/Player/Player";
+        public const string BookSlot = "Level Objects/Interactables/BookSlot";
+        public const string ReadingTable = "Level Objects/Interactables/ReadingTable";
         public const string Hud = "Ui/Hud";
     }
 }
