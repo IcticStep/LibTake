372753bd1bd29d5a565e1d686f605a1c2a7a913e
Maxim Moskalyk <ictic.step@gmail.com>
2023-11-23 18:27:45 +0200

    Customer became first in queue event fixed.

--- LibraryOA/Assets/Code/Runtime/Logic/Customers/QueueMember.cs
+++ LibraryOA/Assets/Code/Runtime/Logic/Customers/QueueMember.cs
@@ -1,23 +1,55 @@
 using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace Code.Runtime.Logic.Customers
 {
     public sealed class QueueMember : MonoBehaviour
     {
-        public Vector3? CurrentPoint { get; private set; }
+        [SerializeField]
+        private float _minDistanceToAchievePoint = 1f;
+        [SerializeField]
+        private float _reachGoalCheckingInterval = 0.1f;
 
+        private Transform _transform;
+        public Vector3? CurrentPoint { get; private set; }
         public bool ActiveMember => CurrentPoint is not null;
         public event Action Updated;
         public event Action BecameFirst;
-        
+
+        private void Awake() =>
+            _transform = transform;
+
+        private void Reset() =>
+            CurrentPoint = null;
+
         public void UpdatePoint(Vector3? point)
         {
             CurrentPoint = point;
             Updated?.Invoke();
         }
 
-        public void InformBecameFirst() =>
+        public async void InformBecameFirst()
+        {
+            await WaitUntilArriveToPoint();
+            if(!ActiveMember)
+                return;
+            
             BecameFirst?.Invoke();
+        }
+
+        private async UniTask WaitUntilArriveToPoint()
+        {
+            do
+            {
+                await UniTask.WaitForSeconds(_reachGoalCheckingInterval);
+            } while(OnTheWayToPoint());
+            return;
+
+            bool OnTheWayToPoint() =>
+                ActiveMember
+                // ReSharper disable once PossibleInvalidOperationException
+                && Vector3.Distance(CurrentPoint.Value, _transform.position) > _minDistanceToAchievePoint;
+        }
     }
 }
