a47a72973f46d3fe3cfa9be5ebd8f15dd1651acd
Maxim Moskalyk <ictic.step@gmail.com>
2023-11-30 23:32:52 +0200

    Books receiver and books receiving interaction service implemented.

---
+++ LibraryOA/Assets/Code/Runtime/Services/Interactions/BooksReceiving/BooksReceivingInteractionsService.cs
@@ -0,0 +1,33 @@
+using Code.Runtime.Logic.Customers;
+using Code.Runtime.Services.BooksReceiving;
+using Code.Runtime.Services.Player;
+using JetBrains.Annotations;
+
+namespace Code.Runtime.Services.Interactions.BooksReceiving
+{
+    [UsedImplicitly]
+    internal sealed class BooksReceivingInteractionsService : IBooksReceivingInteractionsService
+    {
+        private readonly IBooksReceivingService _booksReceivingService;
+        private readonly IPlayerInventoryService _playerInventoryService;
+
+        public BooksReceivingInteractionsService(IBooksReceivingService booksReceivingService, IPlayerInventoryService playerInventoryService)
+        {
+            _booksReceivingService = booksReceivingService;
+            _playerInventoryService = playerInventoryService;
+        }
+
+        public bool CanInteract(BookReceiver bookReceiver) =>
+            _playerInventoryService.HasBook
+            && _playerInventoryService.Count == 1
+            && _playerInventoryService.CurrentBookId == bookReceiver.BookId;
+
+        public void Interact(BookReceiver bookReceiver)
+        {
+            if(!CanInteract(bookReceiver))
+                return;
+            
+            _booksReceivingService.ReceiveBook(bookReceiver.BookId);
+        }
+    }
+}
