05903570781045302bd0dfb06e72c2600cf1c6f2
Maxim Moskalyk <ictic.step@gmail.com>
2023-11-30 22:33:41 +0200

    Customer state machine is being set to BookReceivingState after QueueMemberState; queue dequeueing moved to exit from BookReceivingState.

--- LibraryOA/Assets/Code/Runtime/Logic/Customers/CustomersStates/BookReceivingState.cs
+++ LibraryOA/Assets/Code/Runtime/Logic/Customers/CustomersStates/BookReceivingState.cs
@@ -1,3 +1,4 @@
+using Code.Runtime.Services.CustomersQueue;
 using UnityEngine;
 
 namespace Code.Runtime.Logic.Customers.CustomersStates
@@ -5,19 +6,18 @@ namespace Code.Runtime.Logic.Customers.CustomersStates
     internal class BookReceivingState : ICustomerState
     {
         private readonly CustomerStateMachine _customerStateMachine;
+        private readonly ICustomersQueueService _customersQueueService;
 
-        public BookReceivingState(CustomerStateMachine customerStateMachine)
+        public BookReceivingState(CustomerStateMachine customerStateMachine, ICustomersQueueService customersQueueService)
         {
             _customerStateMachine = customerStateMachine;
+            _customersQueueService = customersQueueService;
         }
 
-        public void Start()
-        {
+        public void Start() =>
             Debug.Log($"Book receiving state entered. Gameobject name: {_customerStateMachine.gameObject.name}");
-        }
-        
-        public void Exit()
-        {
-        }
+
+        public void Exit() =>
+            _customersQueueService.Dequeue();
     }
 }
