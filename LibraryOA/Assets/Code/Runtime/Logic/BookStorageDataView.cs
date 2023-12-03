59c595183d6b4d9ab167ac3646b3c2ebdb5c1e8b
Maxim Moskalyk <ictic.step@gmail.com>
2023-12-03 14:23:07 +0200

    Book storage data view behaviour.

---
+++ LibraryOA/Assets/Code/Runtime/Logic/BookStorageDataView.cs
@@ -0,0 +1,58 @@
+using System;
+using Code.Runtime.Infrastructure.Services.StaticData;
+using Code.Runtime.Logic.Interactions;
+using Code.Runtime.Services.Interactions.ReadBook;
+using Code.Runtime.StaticData.Books;
+using Code.Runtime.Ui;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Code.Runtime.Logic
+{
+    internal sealed class BookStorageDataView : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI _bookName;
+        [SerializeField]
+        private Image _readTick;
+        [SerializeField]
+        private BookStorageHolder _bookStorageHolder;
+        [SerializeField]
+        private SmoothFader _smoothFader;
+        
+        private IStaticDataService _staticDataService;
+        private IReadBookService _readBookService;
+
+        [Inject]
+        private void Construct(IStaticDataService staticDataService, IReadBookService readBookService)
+        {
+            _readBookService = readBookService;
+            _staticDataService = staticDataService;
+        }
+
+        private void Start() =>
+            _smoothFader.FadeImmediately();
+
+        public void ShowData()
+        {
+            if(!_bookStorageHolder.BookStorage.HasBook)
+                return;
+            
+            string bookId = _bookStorageHolder.BookStorage.CurrentBookId;
+            StaticBook bookData = _staticDataService.ForBook(bookId);
+            bool isRead = _readBookService.IsRead(bookId);
+            _bookName.text = bookData.name;
+            _readTick.enabled = isRead;
+            _smoothFader.UnFade();
+        }
+
+        public void HideData()
+        {
+            _bookName.text = string.Empty;
+            _readTick.enabled = false;
+            _smoothFader.Fade();
+        }
+    }
+}
