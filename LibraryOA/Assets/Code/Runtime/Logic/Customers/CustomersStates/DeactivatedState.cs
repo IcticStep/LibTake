0e8b1feeda58742e434ccb9b0de9e5a2472dddf3
Maxim Moskalyk <ictic.step@gmail.com>
2023-12-03 10:32:03 +0200

    Incorrect customers reseting fixed.

--- LibraryOA/Assets/Code/Runtime/Logic/Customers/CustomersStates/DeactivatedState.cs
+++ LibraryOA/Assets/Code/Runtime/Logic/Customers/CustomersStates/DeactivatedState.cs
@@ -7,17 +7,20 @@ namespace Code.Runtime.Logic.Customers.CustomersStates
     {
         private readonly QueueMember _queueMember;
         private readonly BookStorageHolder _bookStorageHolder;
+        private readonly BookReceiver _bookReceiver;
 
-        public DeactivatedState(QueueMember queueMember, BookStorageHolder bookStorageHolder)
+        public DeactivatedState(QueueMember queueMember, BookStorageHolder bookStorageHolder, BookReceiver bookReceiver)
         {
             _queueMember = queueMember;
             _bookStorageHolder = bookStorageHolder;
+            _bookReceiver = bookReceiver;
         }
 
         public void Start()
         {
             _queueMember.Reset();
-            _bookStorageHolder.HardReset();
+            _bookReceiver.Reset();
+            _bookStorageHolder.Reset();
         }
 
         public void Exit()
