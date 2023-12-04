c43d707e083ed032dcb9c35d36462a0e04943db5
Maxim Moskalyk <ictic.step@gmail.com>
2023-12-04 19:14:16 +0200

    Books for delivering moved into truck interaction service.

--- LibraryOA/Assets/Code/Runtime/Services/BooksDelivering/BooksDeliveringService.cs
+++ LibraryOA/Assets/Code/Runtime/Services/BooksDelivering/BooksDeliveringService.cs
@@ -1,8 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
-using Code.Runtime.Data.Progress;
-using Code.Runtime.Infrastructure.Services.PersistentProgress;
 using Code.Runtime.Infrastructure.Services.StaticData;
+using Code.Runtime.Services.Interactions.Truck;
 using Code.Runtime.Services.Random;
 using JetBrains.Annotations;
 
@@ -11,18 +10,18 @@ namespace Code.Runtime.Services.BooksDelivering
     [UsedImplicitly]
     internal sealed class BooksDeliveringService : IBooksDeliveringService
     {
-        private readonly IPersistantProgressService _progressService;
         private readonly IStaticDataService _staticDataService;
         private readonly IRandomService _randomService;
+        private readonly ITruckInteractionService _truckInteractionService;
 
-        private BooksDeliveringData DeliveringData => _progressService.Progress.WorldData.BooksDeliveringData;
-        public int CurrentDayBooksDelivering => _staticDataService.BookReceiving.BooksPerDeliveringAmount;
+        private int CurrentDayBooksDelivering => _staticDataService.BookReceiving.BooksPerDeliveringAmount;
 
-        public BooksDeliveringService(IPersistantProgressService progressService, IStaticDataService staticDataService, IRandomService randomService)
+        public BooksDeliveringService(IStaticDataService staticDataService,
+            IRandomService randomService, ITruckInteractionService truckInteractionService)
         {
-            _progressService = progressService;
             _staticDataService = staticDataService;
             _randomService = randomService;
+            _truckInteractionService = truckInteractionService;
         }
 
         public void DeliverBooksInTruck()
@@ -32,7 +31,7 @@ namespace Code.Runtime.Services.BooksDelivering
             {
                 int chosenIndex = _randomService.GetInRange(0, booksToChoose.Count);
                 string chosenId = booksToChoose[chosenIndex];
-                DeliveringData.AddToPreparedForDelivering(chosenId);
+                _truckInteractionService.PutBookInTruck(chosenId);
             }   
         }
 
