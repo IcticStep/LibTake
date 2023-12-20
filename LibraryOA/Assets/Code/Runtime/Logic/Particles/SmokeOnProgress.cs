f106984a480175f6634761c60a3f25a6622677e9
Maxim Moskalyk <ictic.step@gmail.com>
2023-12-20 18:34:05 +0200

    Smoke is being shown on progress; smoke settings adjusted.

---
+++ LibraryOA/Assets/Code/Runtime/Logic/Particles/SmokeOnProgress.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+namespace Code.Runtime.Logic.Particles
+{
+    internal sealed class SmokeOnProgress : MonoBehaviour
+    {
+        [SerializeField]
+        private ParticleSystem _smoke;
+        [SerializeField]
+        private Progress _progress;
+
+        private void Awake()
+        {
+            _progress.Started += ShowSmoke;
+            _progress.Stopped += HideSmoke;
+        }
+
+        private void Start() =>
+            HideSmoke();
+
+        private void OnDestroy()
+        {
+            _progress.Started -= ShowSmoke;
+            _progress.Stopped -= HideSmoke;
+        }
+
+        private void ShowSmoke()
+        {
+            _smoke.Play();
+        }
+
+        private void HideSmoke()
+        {
+            _smoke.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
+    }
+}
