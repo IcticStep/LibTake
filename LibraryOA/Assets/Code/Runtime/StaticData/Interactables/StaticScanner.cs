4faecc1fedaa921d348b70db6e3cb439ce88ac68
Maxim Moskalyk <ictic.step@gmail.com>
2024-01-05 16:07:22 +0200

    Scanner creatin in interactables factory implemented.

--- LibraryOA/Assets/Code/Runtime/StaticData/Interactables/StaticScanner.cs
+++ LibraryOA/Assets/Code/Runtime/StaticData/Interactables/StaticScanner.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 namespace Code.Runtime.StaticData.Interactables
 {
-    [CreateAssetMenu(fileName = "ReadingTableData", menuName = "Static data/Reading table")]
-    public class StaticReadingTable : ScriptableObject
+    [CreateAssetMenu(fileName = "ScannerData", menuName = "Static data/Scanner")]
+    public class StaticScanner : ScriptableObject
     {
         [field: SerializeField]
         public GameObject Prefab { get; private set; }
 
         [field: SerializeField]
         [field: Range(0.1f, 10000f)]
-        public float SecondsToRead { get; private set; } = 1f;
+        public float SecondsToScan { get; private set; } = 1f;
     }
 }
