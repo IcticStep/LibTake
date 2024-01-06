91c58c58551381f5ac783d7a8c8ef96d87d4ee57
Maxim Moskalyk <ictic.step@gmail.com>
2024-01-06 19:28:38 +0200

    Flying coin amount text added.

--- LibraryOA/Assets/Code/Runtime/Logic/Customers/CustomersStates/RewardState.cs
+++ LibraryOA/Assets/Code/Runtime/Logic/Customers/CustomersStates/RewardState.cs
@@ -1,19 +1,22 @@
+using System;
 using Code.Runtime.Infrastructure.Services.StaticData;
 using Code.Runtime.Logic.Customers.CustomersStates.Api;
 using Code.Runtime.Services.Books.Reward;
 using Code.Runtime.Services.Player.Inventory;
+using Code.Runtime.Ui.Coin;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace Code.Runtime.Logic.Customers.CustomersStates
 {
-    public sealed class RewardState : ICustomerState
+    public sealed class RewardState : ICustomerState, ICoinRewardSource
     {
         private readonly IProgress _progress;
         private readonly ICustomerStateMachine _customerStateMachine;
         private readonly IPlayerInventoryService _playerInventoryService;
         private readonly IBookRewardService _bookRewardService;
         private readonly IStaticDataService _staticDataService;
+        public event Action<int> Rewarded;
 
         public RewardState(ICustomerStateMachine customerStateMachine, IProgress progress, IPlayerInventoryService playerInventoryService,
             IBookRewardService bookRewardService, IStaticDataService staticDataService)
@@ -24,7 +27,7 @@ namespace Code.Runtime.Logic.Customers.CustomersStates
             _bookRewardService = bookRewardService;
             _staticDataService = staticDataService;
         }
-        
+
         public void Start()
         {
             Reward();
@@ -47,6 +50,7 @@ namespace Code.Runtime.Logic.Customers.CustomersStates
         {
             int reward = _bookRewardService.GetRewardBy(_progress);
             _playerInventoryService.AddCoins(reward);
+            Rewarded?.Invoke(reward);
             Debug.Log("Receiving successful. Customer owns a book.");
         }
     }
