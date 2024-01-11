c2e7c325f38158fd33b964f73b78c16d9bb431b4
Maxim Moskalyk <ictic.step@gmail.com>
2024-01-11 12:44:14 +0200

    Static book types extended with icon reference.

--- LibraryOA/Assets/Code/Runtime/StaticData/Books/StaticBookType.cs
+++ LibraryOA/Assets/Code/Runtime/StaticData/Books/StaticBookType.cs
@@ -7,9 +7,14 @@ namespace Code.Runtime.StaticData.Books
     {
         [field: SerializeField]
         public BookType BookType { get; private set; }
+        
         [field: SerializeField]
         public Material Material { get; private set; }
+        
         [field: SerializeField]
         public Color32 UiTextColor { get; private set; }
+        
+        [field: SerializeField]
+        public Sprite Icon { get; private set; }
     }
 }
