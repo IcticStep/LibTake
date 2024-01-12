fd458892406868beadeb0abc965de74ec4fcf62a
Maxim Moskalyk <ictic.step@gmail.com>
2024-01-12 14:16:14 +0200

    Camera follow animated transition.

--- LibraryOA/Assets/Code/Runtime/Logic/CameraControl/CameraFollow.cs
+++ LibraryOA/Assets/Code/Runtime/Logic/CameraControl/CameraFollow.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace Code.Runtime.Logic.CameraControl
@@ -6,10 +7,16 @@ namespace Code.Runtime.Logic.CameraControl
     {
         [SerializeField]
         private Vector3 _offset;
-        
+        [SerializeField]
+        private Ease _ease;
+        [SerializeField]
+        private float _moveDuration;
+
         private Transform _target;
         private Transform _transform;
         
+        private Tween _tween;
+
         public Camera Camera { get; private set; }
 
         private void Awake()
@@ -20,11 +27,51 @@ namespace Code.Runtime.Logic.CameraControl
 
         private void LateUpdate()
         {
-            if (_target != null)
-                _transform.position = _target.position + _offset;
+            if(_tween is not null)
+                return;
+            
+            GoToTargetImmediately();
         }
 
-        public void SetTarget(Transform target) =>
+        private void GoToTargetImmediately()
+        {
+            if (_target == null)
+                return;
+            
+            _transform.position = GetPositionByTarget(_target);
+        }
+
+        public void SetTarget(Transform target)
+        {
             _target = target;
+            AnimateTransition();
+        }
+
+        private void AnimateTransition()
+        {
+            KillPreviousAnimationIfAny();
+            _tween = CreateTransitionTween();
+        }
+
+        private void KillPreviousAnimationIfAny()
+        {
+            if(_tween is null)
+                return;
+
+            _tween.Kill();
+            _tween = null;
+        }
+
+        private Tween CreateTransitionTween() =>
+            transform
+                .DOMove(GetPositionByTarget(_target), _moveDuration)
+                .SetEase(_ease)
+                .OnComplete(NullTween);
+
+        private Vector3 GetPositionByTarget(Transform target) =>
+            target.position + _offset;
+
+        private void NullTween() =>
+            _tween = null;
     }
 }
