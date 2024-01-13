202d689e9fe3c346cedcb1694c31dc6b236a9356
Maxim Moskalyk <ictic.step@gmail.com>
2024-01-13 19:01:25 +0200

    Crafting table crafting state ui.

---
+++ LibraryOA/Assets/Code/Runtime/Logic/Interactables/Crafting/Ui/StatesCanvases/Crafting/CraftingStateCanvasView.cs
@@ -0,0 +1,6 @@
+using Code.Runtime.Logic.Interactables.Crafting.CraftingTableStates;
+
+namespace Code.Runtime.Logic.Interactables.Crafting.Ui.StatesCanvases.Crafting
+{
+    internal sealed class CraftingStateCanvasView : CanvasViewForState<CraftingState> { }
+}
