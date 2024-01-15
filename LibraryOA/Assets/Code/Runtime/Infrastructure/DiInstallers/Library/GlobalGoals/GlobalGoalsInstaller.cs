fe1f372c39412b5f58a5a8c8def325f062044506
Maxim Moskalyk <ictic.step@gmail.com>
2024-01-15 21:39:44 +0200

    Global goals and steps visualization schemes data refactoring.

--- LibraryOA/Assets/Code/Runtime/Infrastructure/DiInstallers/Library/GlobalGoals/GlobalGoalsInstaller.cs
+++ LibraryOA/Assets/Code/Runtime/Infrastructure/DiInstallers/Library/GlobalGoals/GlobalGoalsInstaller.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
+using Code.Runtime.Infrastructure.DiInstallers.Library.GlobalGoals.Data;
 using Zenject;
 
 namespace Code.Runtime.Infrastructure.DiInstallers.Library.GlobalGoals
 {
     public sealed class GlobalGoalsInstaller : MonoInstaller, IInitializable
     {
-        public List<GlobalGoalVisualizationScheme> GlobalGoalsVisualizationSchemes;
+        public List<GlobalGoalScheme> GlobalGoalsVisualizationSchemes;
 
         public override void InstallBindings() =>
             Container.BindInterfacesAndSelfTo<GlobalGoalsInstaller>().FromInstance(this);
