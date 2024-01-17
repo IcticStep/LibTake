126542c9c922d464148650bd4111d561628e2970
Maxim Moskalyk <ictic.step@gmail.com>
2024-01-17 15:16:04 +0200

    Global steps visualizers camera target option added.

--- LibraryOA/Assets/Code/Runtime/Infrastructure/DiInstallers/Library/GlobalGoals/Data/GlobalStepScheme.cs
+++ LibraryOA/Assets/Code/Runtime/Infrastructure/DiInstallers/Library/GlobalGoals/Data/GlobalStepScheme.cs
@@ -13,15 +13,20 @@ namespace Code.Runtime.Infrastructure.DiInstallers.Library.GlobalGoals.Data
         private GlobalStep _globalStep;
         
         [SerializeField]
+        private GlobalStepPartVisualizer _cameraTarget;
+
+        [SerializeField]
         private List<GlobalStepPartVisualizer> _visualizers;
 
         public List<GlobalStepPartVisualizer> Visualizers => _visualizers;
         public GlobalStep Step => _globalStep;
+        public GlobalStepPartVisualizer CameraTarget => _cameraTarget;
 
-        public GlobalStepScheme(GlobalStep globalStep, List<GlobalStepPartVisualizer> visualizers)
+        public GlobalStepScheme(GlobalStep globalStep, List<GlobalStepPartVisualizer> visualizers, GlobalStepPartVisualizer cameraTarget)
         {
             _globalStep = globalStep;
             _visualizers = visualizers;
+            _cameraTarget = cameraTarget;
         }
 
         public override string ToString() =>
