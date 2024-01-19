7bfb1948379958a8a10bdc6996e657e4b1164a17
Maxim Moskalyk <ictic.step@gmail.com>
2024-01-19 16:46:55 +0200

    Book scanning stop on scanning book permision denied implemented.

--- LibraryOA/Assets/Code/Runtime/Services/Interactions/Scanning/IScanBookService.cs
+++ LibraryOA/Assets/Code/Runtime/Services/Interactions/Scanning/IScanBookService.cs
@@ -1,4 +1,5 @@
 using System;
+using Code.Runtime.Data.Progress;
 using Code.Runtime.Infrastructure.Services.SaveLoad;
 
 namespace Code.Runtime.Services.Interactions.Scanning
@@ -7,10 +8,13 @@ namespace Code.Runtime.Services.Interactions.Scanning
     {
         bool ScanningAllowed { get; }
         event Action BookScanned;
+        event Action<bool> ScanningPermissionChanged;
         void AllowScanning();
         void BlockScanning();
         bool IsScanned(string bookId);
         void ScanBook(string bookId);
         bool CanScanBook(string bookId);
+        void LoadProgress(GameProgress progress);
+        void UpdateProgress(GameProgress progress);
     }
 }
