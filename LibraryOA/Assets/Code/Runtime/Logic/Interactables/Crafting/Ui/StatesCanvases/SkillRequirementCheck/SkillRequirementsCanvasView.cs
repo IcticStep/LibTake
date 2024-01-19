90c90573835d808d43a6c8232209a2f34f3c11c6
Maxim Moskalyk <ictic.step@gmail.com>
2024-01-19 14:18:47 +0200

    Skills requierment completed is being update each time on hover when crafting table is in skill check state.

--- LibraryOA/Assets/Code/Runtime/Logic/Interactables/Crafting/Ui/StatesCanvases/SkillRequirementCheck/SkillRequirementsCanvasView.cs
+++ LibraryOA/Assets/Code/Runtime/Logic/Interactables/Crafting/Ui/StatesCanvases/SkillRequirementCheck/SkillRequirementsCanvasView.cs
@@ -2,14 +2,32 @@ using System;
 using System.Collections.Generic;
 using Code.Runtime.Logic.Interactables.Crafting.CraftingTableStates;
 using Code.Runtime.StaticData.GlobalGoals;
+using UnityEngine;
 
 namespace Code.Runtime.Logic.Interactables.Crafting.Ui.StatesCanvases.SkillRequirementCheck
 {
     internal sealed class SkillRequirementsCanvasView : CanvasViewForState<SkillCheckState>
     {
-        public event Action<IReadOnlyList<SkillConstraint>> RequirementsUpdated;
+        [SerializeField]
+        private CraftingTableStateMachine _craftingTable;
+        
+        public event Action<IReadOnlyList<SkillConstraint>> RequirementsRequested;
+
+        protected override void OnAwaking() =>
+            _craftingTable.Hovered += OnHovered;
+
+        protected override void OnDestroying() =>
+            _craftingTable.Hovered -= OnHovered;
+
+        private void OnHovered()
+        {
+            if(_craftingTable.ActiveState is not SkillCheckState skillCheckState)
+                return;
+            
+            RequirementsRequested?.Invoke(skillCheckState.SkillRequirements);
+        }
 
         protected override void OnCanvasShow(SkillCheckState state) =>
-            RequirementsUpdated?.Invoke(state.SkillRequirements);
+            RequirementsRequested?.Invoke(state.SkillRequirements);
     }
 }
