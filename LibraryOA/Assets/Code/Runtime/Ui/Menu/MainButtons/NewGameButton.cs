248a6f3a06f4ed3aca7167c7f73a6c763194ed91
Maxim Moskalyk <ictic.step@gmail.com>
2024-01-31 21:37:12 +0200

    Global goal animation implemented.

--- LibraryOA/Assets/Code/Runtime/Ui/Menu/MainButtons/NewGameButton.cs
+++ LibraryOA/Assets/Code/Runtime/Ui/Menu/MainButtons/NewGameButton.cs
@@ -1,7 +1,10 @@
 using Code.Runtime.Data;
 using Code.Runtime.Infrastructure.GameStates;
 using Code.Runtime.Infrastructure.GameStates.States;
+using Code.Runtime.Ui.Menu.Common;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 using Zenject;
 
@@ -11,20 +14,28 @@ namespace Code.Runtime.Ui.Menu.MainButtons
     {
         [SerializeField]
         private Button _button;
-        
-        private GameStateMachine _gameStateMachine;
-
-        [Inject]
-        private void Construct(GameStateMachine gameStateMachine) =>
-            _gameStateMachine = gameStateMachine;
+        [SerializeField]
+        private GlobalGoalsContainer _globalsGoalsContainer;
+        [SerializeField]
+        private MainButtonsGroup _mainButtonsGroup;
+        [SerializeField]
+        private GameName _gameName;
 
         private void Awake() =>
-            _button.onClick.AddListener(StartNewGame);
+            _button.onClick.AddListener(OnNewGameButton);
 
         private void OnDestroy() =>
-            _button.onClick.RemoveListener(StartNewGame);
+            _button.onClick.RemoveListener(OnNewGameButton);
 
-        private void StartNewGame() =>
-            _gameStateMachine.EnterState<LoadProgressState, LoadProgressOption>(LoadProgressOption.ForceCreatingNewProgress);
+        private void OnNewGameButton() =>
+            ShowGoalsSelection()
+                .Forget();
+
+        private async UniTaskVoid ShowGoalsSelection()
+        {
+            _globalsGoalsContainer.Show().Forget();
+            _gameName.Hide().Forget();
+            await _mainButtonsGroup.Hide();
+        }
     }
 }
