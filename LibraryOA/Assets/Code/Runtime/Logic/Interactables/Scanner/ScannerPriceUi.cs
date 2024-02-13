38847a92c4f146a1452b080f2e013bba6fe893f2
Maxim Moskalyk <ictic.step@gmail.com>
2024-02-13 12:41:07 +0200

    Scanner show price on hover implemented.

--- LibraryOA/Assets/Code/Runtime/Logic/Interactables/Scanner/ScannerPriceUi.cs
+++ LibraryOA/Assets/Code/Runtime/Logic/Interactables/Scanner/ScannerPriceUi.cs
@@ -1,4 +1,5 @@
 using Code.Runtime.Infrastructure.Services.StaticData;
+using Code.Runtime.Ui.Common;
 using TMPro;
 using UnityEngine;
 using Zenject;
@@ -9,13 +10,34 @@ namespace Code.Runtime.Logic.Interactables.Scanner
     {
         [SerializeField]
         private TextMeshProUGUI _priceText;
+        [SerializeField]
+        private HoverView _hoverView;
+        [SerializeField]
+        private SmoothFader _smoothFader;
+        
         private IStaticDataService _staticDataService;
 
         [Inject]
         private void Construct(IStaticDataService staticDataService) =>
             _staticDataService = staticDataService;
 
-        private void Start() =>
+        private void Start()
+        {
             _priceText.text = "x" + _staticDataService.Interactables.Scanner.CoinsReward;
+            _hoverView.Focused += OnHoverFocused;
+            _hoverView.Unfocused += OnHoverUnfocused;
+        }
+
+        private void OnDestroy()
+        {
+            _hoverView.Focused -= OnHoverFocused;
+            _hoverView.Unfocused -= OnHoverUnfocused;
+        }
+
+        private void OnHoverFocused() =>
+            _smoothFader.UnFade();
+
+        private void OnHoverUnfocused() =>
+            _smoothFader.Fade();
     }
 }
