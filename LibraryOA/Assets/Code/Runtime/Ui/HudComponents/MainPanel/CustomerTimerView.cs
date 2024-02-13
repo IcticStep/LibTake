c3f8e4ee839684d138f1c6072e57ac4fcbe473bf
Maxim Moskalyk <ictic.step@gmail.com>
2024-02-13 13:33:15 +0200

    Hud customer timer view refactored; clockwise changed to non-clockwise for consistency.

--- LibraryOA/Assets/Code/Runtime/Ui/HudComponents/MainPanel/CustomerTimerView.cs
+++ LibraryOA/Assets/Code/Runtime/Ui/HudComponents/MainPanel/CustomerTimerView.cs
@@ -1,12 +1,5 @@
-using System.Collections;
-using Code.Runtime.Logic;
-using Code.Runtime.Logic.Customers;
-using Code.Runtime.Logic.Customers.CustomersStates;
-using Code.Runtime.Services.Customers.Queue;
-using Code.Runtime.Services.Customers.Registry;
 using Code.Runtime.Ui.Common.Progress;
 using UnityEngine;
-using Zenject;
 
 namespace Code.Runtime.Ui.HudComponents.MainPanel
 {
@@ -15,58 +8,15 @@ namespace Code.Runtime.Ui.HudComponents.MainPanel
         [SerializeField]
         private ProgressBar _progressBar;
         [SerializeField]
-        private float _updateInterval = 0.1f;
-
-        private ICustomersQueueService _customersQueueService;
-        private ICustomersRegistryService _customersRegistryService;
-        private WaitForSeconds _waitCached;
-
-        [Inject]
-        private void Construct(ICustomersQueueService customersQueueService,
-            ICustomersRegistryService customersRegistryService)
-        {
-            _customersRegistryService = customersRegistryService;
-            _customersQueueService = customersQueueService;
-        }
+        private CustomersTimerSource _customersTimerSource;
 
         private void Awake() =>
-            _waitCached = new WaitForSeconds(_updateInterval);
+            _customersTimerSource.Updated += OnCustomersTimerSourceUpdated;
+        
+        private void OnDestroy() =>
+            _customersTimerSource.Updated -= OnCustomersTimerSourceUpdated;
 
-        private void Start() =>
-            StartCoroutine(UpdateViewCoroutine());
-
-        private IEnumerator UpdateViewCoroutine()
-        {
-            while(true)
-            {
-                yield return _waitCached;
-                UpdateProgressBar();
-            }
-        }
-
-        private void UpdateProgressBar()
-        {
-            if(!_customersQueueService.Any)
-            {
-                _progressBar.SetProgress(0, 1);
-                return;
-            }
-
-            ICustomerStateMachine firstCustomer = GetFirstCustomer();
-            if(firstCustomer.ActiveStateType != typeof(BookReceivingState))
-            {
-                _progressBar.SetProgress(0, 1);
-                return;
-            }
-
-            IProgress progress = firstCustomer.Progress;
-            _progressBar.SetProgress(progress.Value, progress.MaxValue);
-        }
-
-        private ICustomerStateMachine GetFirstCustomer()
-        {
-            QueueMember firstCustomerMember = _customersQueueService.Peek();
-            return _customersRegistryService.GetCustomerByQueueMember(firstCustomerMember);
-        }
+        private void OnCustomersTimerSourceUpdated(float value) =>
+            _progressBar.SetProgress(value, 1);
     }
 }
