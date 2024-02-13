84ffd4d16aa2dc575407b6d4b3e0bb3af7782b75
Maxim Moskalyk <ictic.step@gmail.com>
2024-02-13 13:22:55 +0200

    Hud panels ui punch animations added.

--- LibraryOA/Assets/Code/Runtime/Ui/HudComponents/MainPanel/LivesView.cs
+++ LibraryOA/Assets/Code/Runtime/Ui/HudComponents/MainPanel/LivesView.cs
@@ -1,6 +1,8 @@
 using Code.Runtime.Services.Player.Lives;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Zenject;
 
 namespace Code.Runtime.Ui.HudComponents.MainPanel
@@ -9,13 +11,18 @@ namespace Code.Runtime.Ui.HudComponents.MainPanel
     {
         [SerializeField]
         private TextMeshProUGUI _text;
-        
+        [SerializeField]
+        private RectTransform _animationTarget;
+
         private IPlayerLivesService _livesService;
 
         [Inject]
         private void Construct(IPlayerLivesService livesService) =>
             _livesService = livesService;
 
+        private void OnValidate() =>
+            _animationTarget ??= GetComponent<RectTransform>();
+        
         private void Start()
         {
             _livesService.Updated += UpdateView;
@@ -25,7 +32,10 @@ namespace Code.Runtime.Ui.HudComponents.MainPanel
         private void OnDestroy() =>
             _livesService.Updated -= UpdateView;
 
-        private void UpdateView() =>
+        private void UpdateView()
+        {
             _text.text = _livesService.Lives.ToString();
+            _animationTarget.DOPunchScale(Vector3.one * 1.1f, 0.5f, 1, 0.5f);
+        }
     }
 }
--- LibraryOA/Assets/Code/Runtime/Ui/HudComponents/SkillView.cs
+++ LibraryOA/Assets/Code/Runtime/Ui/HudComponents/SkillView.cs
@@ -2,8 +2,10 @@ using System.Linq;
 using Code.Runtime.Infrastructure.Services.StaticData;
 using Code.Runtime.Services.Skills;
 using Code.Runtime.StaticData.Books;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 using Zenject;
 
@@ -17,10 +19,14 @@ namespace Code.Runtime.Ui.HudComponents
         private Image _iconImage;
         [SerializeField]
         private BookType _bookType;
+        [FormerlySerializedAs("_rectTransform")]
+        [SerializeField]
+        private RectTransform _animationTarget;
         
         private IPlayerSkillService _playerSkillService;
         private IStaticDataService _staticDataService;
         private StaticBookType _staticBookType;
+        private int _skillValue;
 
         [Inject]
         private void Construct(IPlayerSkillService playerSkillService, IStaticDataService staticDataService)
@@ -50,6 +56,17 @@ namespace Code.Runtime.Ui.HudComponents
             string textText = skill.ToString();
             _text.text = textText;
             _iconImage.sprite = _staticBookType.Icon;
+
+            PlayAnimationIfChanged(skill);
+        }
+
+        private void PlayAnimationIfChanged(int skill)
+        {
+            if(_skillValue == skill)
+                return;
+            
+            _animationTarget.DOPunchScale(Vector3.one * 1.1f, 0.5f, 1, 0.5f);
+            _skillValue = skill;
         }
     }
 }
