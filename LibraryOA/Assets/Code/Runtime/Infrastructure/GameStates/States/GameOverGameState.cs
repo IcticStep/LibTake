07384edd023b9d7ab56289ffac53f54b87b990c9
Maxim Moskalyk <ictic.step@gmail.com>
2024-02-15 16:43:25 +0200

    Added black loading curtain on game over.

--- LibraryOA/Assets/Code/Runtime/Infrastructure/GameStates/States/GameOverGameState.cs
+++ LibraryOA/Assets/Code/Runtime/Infrastructure/GameStates/States/GameOverGameState.cs
@@ -2,6 +2,8 @@ using Code.Runtime.Infrastructure.GameStates.Api;
 using Code.Runtime.Infrastructure.Services.CleanUp;
 using Code.Runtime.Infrastructure.Services.SceneMenegment;
 using Code.Runtime.Infrastructure.Services.StaticData;
+using Code.Runtime.Services.Loading;
+using Cysharp.Threading.Tasks;
 using JetBrains.Annotations;
 
 namespace Code.Runtime.Infrastructure.GameStates.States
@@ -13,22 +15,31 @@ namespace Code.Runtime.Infrastructure.GameStates.States
         private readonly IStaticDataService _staticDataService;
         private readonly ILevelCleanUpService _levelCleanUpService;
         private readonly GameStateMachine _gameStateMachine;
+        private readonly ILoadingCurtainService _loadingCurtainService;
 
         public GameOverGameState(ISceneLoader sceneLoader, IStaticDataService staticDataService, ILevelCleanUpService levelCleanUpService,
-            GameStateMachine gameStateMachine)
+            GameStateMachine gameStateMachine, ILoadingCurtainService loadingCurtainService)
         {
             _sceneLoader = sceneLoader;
             _staticDataService = staticDataService;
             _levelCleanUpService = levelCleanUpService;
             _gameStateMachine = gameStateMachine;
+            _loadingCurtainService = loadingCurtainService;
         }
 
         public void Start()
         {
             _gameStateMachine.EnterState<MenuGameState>();
-            
+
+            GoToGameOverScene().Forget();
+        }
+
+        private async UniTaskVoid GoToGameOverScene()
+        {
             string gameOverScene = _staticDataService.ScenesRouting.GameOverScene;
-            _sceneLoader.LoadSceneAsync(gameOverScene, OnSceneLoaded);
+            await _loadingCurtainService.ShowBlackAsync();
+            await _sceneLoader.LoadSceneAsync(gameOverScene, OnSceneLoaded);
+            await _loadingCurtainService.HideBlackAsync();
         }
 
         private void OnSceneLoaded() =>
