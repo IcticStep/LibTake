cfbda5b01572829fe07b1d84a12a28e28c08f6a3
Maxim Moskalyk <ictic.step@gmail.com>
2024-08-27 21:09:25 +0300

    Added test that opens main menu and press "new game" button.

---
+++ LibraryOA/Assets/Code/PlayModeTests/Extensions/ButtonExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.UI;
+
+namespace Code.PlayModeTests.Extensions
+{
+    internal static class ButtonExtensions
+    {
+        public static void SimulateClick(this Button button) =>
+            button.onClick.Invoke();
+    }
+}
--- LibraryOA/Assets/Code/PlayModeTests/PlayModeExceptionsTests.cs
+++ LibraryOA/Assets/Code/PlayModeTests/PlayModeExceptionsTests.cs
@@ -1,8 +1,15 @@
+using System;
 using System.Collections;
+using System.Linq;
+using Code.PlayModeTests.Extensions;
+using Code.Runtime.Ui.Menu.MainButtons;
 using Cysharp.Threading.Tasks;
 using NUnit.Framework;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
 
 namespace Code.PlayModeTests
 {
@@ -13,11 +20,27 @@ namespace Code.PlayModeTests
             UniTask.ToCoroutine(
                 async () =>
                 {
-                    // Arrange.
                     await SceneManager.LoadSceneAsync(0);
-
-                    // Act.
                     await UniTask.WaitForSeconds(1f);
                 });
+        
+        [UnityTest]
+        public IEnumerator WhenStartedGame_AndWaited_ThenNoExceptionsThrown() =>
+            UniTask.ToCoroutine(
+                async () =>
+                {
+                    await SceneManager.LoadSceneAsync(0);
+                    await UniTask.WaitForSeconds(1f);
+
+                    FindNewGameButton().SimulateClick();
+                    await UniTask.WaitForSeconds(1f); 
+                });
+
+        private static Button FindNewGameButton() =>
+            Object
+                .FindObjectsByType<NewGameButton>(FindObjectsInactive.Exclude, FindObjectsSortMode.None)
+                .First()
+                .gameObject
+                .GetComponent<Button>();
     }
 }
