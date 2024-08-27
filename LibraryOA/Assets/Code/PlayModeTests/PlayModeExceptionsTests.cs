40f7c629f809e7efc614dd8461f5668fcad3ba3f
Maxim Moskalyk <ictic.step@gmail.com>
2024-08-27 21:35:00 +0300

    Playmode tests naming refactoring.

--- LibraryOA/Assets/Code/PlayModeTests/PlayModeExceptionsTests.cs
+++ LibraryOA/Assets/Code/PlayModeTests/PlayModeExceptionsTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Linq;
 using Code.PlayModeTests.Extensions;
+using Code.Runtime.Ui.Menu.GlobalGoals;
 using Code.Runtime.Ui.Menu.MainButtons;
 using Cysharp.Threading.Tasks;
 using NUnit.Framework;
@@ -15,8 +16,10 @@ namespace Code.PlayModeTests
 {
     public class PlayModeExceptionsTests
     {
+        // To do: Refactor tests to use more high-level abstractions
+        
         [UnityTest]
-        public IEnumerator WhenNavigatedToMainMenu_ThenNoExceptionsThrown() =>
+        public IEnumerator OpenMainMenu() =>
             UniTask.ToCoroutine(
                 async () =>
                 {
@@ -25,16 +28,24 @@ namespace Code.PlayModeTests
                 });
         
         [UnityTest]
-        public IEnumerator WhenStartedGame_AndWaited_ThenNoExceptionsThrown() =>
+        public IEnumerator OpenMainMenu_StartGame() =>
             UniTask.ToCoroutine(
                 async () =>
                 {
                     await SceneManager.LoadSceneAsync(0);
-                    await UniTask.WaitForSeconds(1f);
+                    await UniTask.WaitForSeconds(2f);
 
                     FindNewGameButton().SimulateClick();
-                    await UniTask.WaitForSeconds(1f); 
+                    await UniTask.WaitForSeconds(2f);
+
+                    FindGlobalGoalButton().SimulateClick();
+                    await UniTask.WaitForSeconds(5f);
                 });
+
+        private static Button FindGlobalGoalButton() =>
+            Object
+                .FindObjectOfType<GlobalGoalUiView>()
+                .GetComponentInChildren<Button>();
 
         private static Button FindNewGameButton() =>
             Object
